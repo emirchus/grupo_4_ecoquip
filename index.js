930dea58072c535ff7b0aecaf4a4e4c1e849e2df
Emir Ali <emirhassan.ali02@gmail.com>
2023-07-14 22:40:37 -0300

    feat: creo las rutas

--- index.js
+++ index.js
@@ -8,13 +8,22 @@ const { env } = require("process");
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
+
+const homeRoute = require("./routes/home");
+app.use("/", homeRoute);
+
 const signInRoute = require("./routes/signin");
-
 app.use("/signin", signInRoute);
-
 
 const signUpRoute = require("./routes/signup");
 app.use("/signup", signUpRoute);
+
+
+const cartRoute = require("./routes/cart");
+app.use("/cart", cartRoute);
+
+const productRoute = require("./routes/product");
+app.use("/product", productRoute);
 
 app.listen(env.PORT || 3000, () =>
   console.log(`Servidor levantado en puerto ${env.PORT || 3000} 🚀`)
