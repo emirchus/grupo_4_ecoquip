123003bc264453cacaa0f69e28e7997ee9043e80
Emir Ali <emirhassan.ali02@gmail.com>
2023-07-14 22:58:03 -0300

    feat: se añade marca

--- index.js
+++ index.js
@@ -5,25 +5,34 @@ const path = require("path");
 
 const { env } = require("process");
 
-const publicPath = path.resolve(__dirname, "./public");
+const publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
+// const homeRoute = require("./routes/home");
+// app.use("/", homeRoute);
 
-const homeRoute = require("./routes/home");
-app.use("/", homeRoute);
+// const signInRoute = require("./routes/signin");
+// app.use("/signin", signInRoute);
 
-const signInRoute = require("./routes/signin");
-app.use("/signin", signInRoute);
+// const signUpRoute = require("./routes/signup");
+// app.use("/signup", signUpRoute);
 
-const signUpRoute = require("./routes/signup");
-app.use("/signup", signUpRoute);
+// const cartRoute = require("./routes/cart");
+// app.use("/cart", cartRoute);
 
+// const productRoute = require("./routes/product");
+// app.use("/product", productRoute);
 
-const cartRoute = require("./routes/cart");
-app.use("/cart", cartRoute);
+const fs = require("fs");
 
-const productRoute = require("./routes/product");
-app.use("/product", productRoute);
+const routesPath = path.resolve(__dirname, "routes");
+
+const routes = fs.readdirSync(routesPath);
+
+routes.forEach((route) => {
+  const routePath = path.resolve(__dirname, "routes", route);
+  app.use(`/${route.split`.`[0]}`, require(routePath));
+});
 
 app.listen(env.PORT || 3000, () =>
   console.log(`Servidor levantado en puerto ${env.PORT || 3000} 🚀`)
