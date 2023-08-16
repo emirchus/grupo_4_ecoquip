945693264bea27957da259c7a0996bca1f928623
Veronica <verdani8@gmail.com>
2023-08-15 21:09:13 -0300

    subí controller y router y modifiqué app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mainRouter = require('./routes/mainRouter')
 const path = require("path");
 
 app.use(express.static('public'));
@@ -9,31 +10,5 @@ app.set('view engine', 'ejs');
 
 app.listen(3001, () => console.log("servidor 3001 funcionando"));
 
-app.get("/", (req, res) => {
-  res.render('home');
-});
-
-app.get("/login", (req, res) => {
-  res.render('login');
-});
-
-app.get("/productCart", (req, res) => {
-  res.render('productCart');
-});
-
-app.get("/productDetail", (req, res) => {
-  res.render('productDetail');
-});
-
-app.get("/register", (req, res) => {
-  res.render('register');
-});
-
-app.get("/header", (req, res) => {
-  res.render('header');
-});
-
-app.get("/header2", (req, res) => {
-  res.render('header2');
-});
+app.use(mainRouter)
 
