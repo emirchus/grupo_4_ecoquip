163d06211a5daa3f9655fcb27dbf9a01352f8bb6
hernancarreno01 <radioportatil@gmail.com>
2023-08-17 20:19:43 -0300

    fix register error en ejs

--- src/app.js
+++ src/app.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const mainRouter = require('./routes/mainRouter')
 const path = require("path");
+const methodOverride = require('method-override');
 
 app.use(express.static('public'));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.listen(3001, () => console.log("servidor 3001 funcionando"));
 
 app.use(mainRouter)
+
+app.use(methodOverride('_method'));
 
