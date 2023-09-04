fe71f6a4f3d3b92b9faf4047ccb3c906a784d071
Veronica <verdani8@gmail.com>
2023-09-03 22:02:20 -0300

    crear y modificar no funcionan del todo

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const multer = require('multer');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,7 +19,7 @@ app.listen(3001, () => console.log("servidor 3001 funcionando"));
 
 app.use(mainRouter)
 
-app.use(methodOverride('_method'));
+
 
 
 
