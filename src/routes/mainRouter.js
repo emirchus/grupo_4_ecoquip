85b9f171cd8a35d9222635997a29f663bd5601ee
Veronica <verdani8@gmail.com>
2023-09-04 21:03:11 -0300

    delete y recuperar poductos ok

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController')
 
-
-
 router.get("/", mainController.home);
 router.get("/login", mainController.login);
 router.get("/productCart", mainController.productCart);
@@ -14,8 +12,10 @@ router.get("/header2", mainController.header2);
 router.get("/productCreate", mainController.productCreate);
 router.get("/productModify/:id", mainController.productModify);
 
-
 router.post("/productCreate", mainController.productCreateProcess);
 router.put("/productModify/:id", mainController.productModifyProcess);
+router.delete("/productDelete/:id", mainController.productDelete);
+router.put("/productAlta/:id", mainController.recuperarProcess);
+
 
 module.exports = router
